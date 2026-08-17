f4ab2a0e42616f178e28d90274f9b9347a0708dc
marcuandreistefan <139911804+marcuandreistefan@users.noreply.github.com>
2026-08-17 15:02:11 +0200

    Add configurable options flow

--- custom_components/fake_climate/config_flow.py
+++ custom_components/fake_climate/config_flow.py
@@ -1,8 +1,17 @@
 """Config flow for Fake Climate Device."""
 
-from homeassistant import config_entries
+import voluptuous as vol
 
-from .const import DOMAIN
+from homeassistant import config_entries
+from homeassistant.core import callback
+
+from .const import (
+    CONF_HUMIDITY,
+    CONF_TEMPERATURE,
+    DEFAULT_HUMIDITY,
+    DEFAULT_TEMPERATURE,
+    DOMAIN,
+)
 
 
 class FakeClimateConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -24,4 +33,51 @@ class FakeClimateConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
         return self.async_show_form(
             step_id="user",
+        )
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry):
+        """Create the options flow."""
+        return FakeClimateOptionsFlow()
+
+
+class FakeClimateOptionsFlow(config_entries.OptionsFlow):
+    """Handle Fake Climate Device options."""
+
+    async def async_step_init(self, user_input=None):
+        """Manage the options."""
+
+        if user_input is not None:
+            return self.async_create_entry(
+                title="",
+                data=user_input,
+            )
+
+        current_temperature = self.config_entry.options.get(
+            CONF_TEMPERATURE,
+            DEFAULT_TEMPERATURE,
+        )
+
+        current_humidity = self.config_entry.options.get(
+            CONF_HUMIDITY,
+            DEFAULT_HUMIDITY,
+        )
+
+        schema = vol.Schema(
+            {
+                vol.Required(
+                    CONF_TEMPERATURE,
+                    default=current_temperature,
+                ): vol.Coerce(float),
+                vol.Required(
+                    CONF_HUMIDITY,
+                    default=current_humidity,
+                ): vol.Coerce(float),
+            }
+        )
+
+        return self.async_show_form(
+            step_id="init",
+            data_schema=schema,
         )
