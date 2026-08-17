56efe8d9274d8f155b5e8e526a4edeccb8f70b60
marcuandreistefan <139911804+marcuandreistefan@users.noreply.github.com>
2026-08-17 14:59:09 +0200

    Add configurable sensor constants

--- custom_components/fake_climate/const.py
+++ custom_components/fake_climate/const.py
@@ -5,3 +5,9 @@ DOMAIN = "fake_climate"
 DEVICE_NAME = "Fake Climate Device"
 MANUFACTURER = "Marcu Test Lab"
 MODEL = "Virtual TH Sensor"
+
+CONF_TEMPERATURE = "temperature"
+CONF_HUMIDITY = "humidity"
+
+DEFAULT_TEMPERATURE = 24.0
+DEFAULT_HUMIDITY = 50.0
